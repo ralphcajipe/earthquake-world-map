b7a1fd5bbac9c0b3d75aec1ae8a3524305a7d930
Stephencamilon <113228328+Stephencamilon@users.noreply.github.com>
2024-02-14 20:57:10 +0800

    Here is my EQ-2, EQ-3 and EQ-4. The files are updated.

---
+++ data/eq_explore_data.py
@@ -0,0 +1,32 @@
+import json
+
+filename = 'data/eq_data_1_day_m4_5.json'
+
+# Open the JSON file with utf-8 encoding
+with open(filename, 'r', encoding='utf-8') as f:
+    all_eq_data = json.load(f)
+
+# Create a new JSON file with formatted data
+readable_filename = 'data/readable_eq_data.json'
+with open(readable_filename, 'w', encoding='utf-8') as f:
+    # Dump the Python object into the new JSON file, with ASCII characters ensured and indented by 4 spaces
+    json.dump(all_eq_data, f, ensure_ascii=False, indent=4)
+
+# Open the new JSON file and display the data
+with open(readable_filename, 'r', encoding='utf-8') as f:
+    readable_eq_data = json.load(f)
+    print(readable_eq_data)
+
+# Extract the earthquake magnitude data
+all_eq_dicts = all_eq_data['features']
+print(len(all_eq_dicts))
+
+mags = []
+
+# Extract the magnitude of each earthquake
+for eq_dict in all_eq_dicts:
+    mag = eq_dict['properties']['mag']
+    mags.append(mag)
+
+# Display the first 5 magnitudes
+print(mags[:5])
